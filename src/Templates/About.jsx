16570abbc42171800818300a7011aec018a7c7ad
DESKTOP-3DOP2EC\ADMIN <rinkusalunke9@gmail.com>
2025-04-02 15:35:20 +0530

    1st commit

---
+++ src/Templates/About.jsx
@@ -0,0 +1,19 @@
+const { Component } = require("react");
+
+class About extends Component{
+    constructor(){
+        super();
+    }
+
+
+    render(){
+        return <>
+        <h1>About Component </h1>
+        <p>About is a class component which is extending component class from React Library</p>
+        </>
+    }
+
+}
+
+export default About;
+
